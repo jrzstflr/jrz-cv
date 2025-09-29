47c4bae6dec9afc8fd1159e459f1924c3fdd2d90
jrzstflr <jrzstflr@outlook.com>
2025-09-29 15:46:12 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Services from './components/Services';
-import Work from './components/Work';
-import Contact from './components/Contact';
-import ChatBot from './components/ChatBot';
-import RatingTestimonial from './components/RatingTestimonial';
-import TestimonialList from './components/TestimonialList';
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Services from "./components/Services"
+import Work from "./components/Work"
+import Contact from "./components/Contact"
+import ChatBot from "./components/ChatBot"
+import RatingTestimonial from "./components/RatingTestimonial"
+import TestimonialList from "./components/TestimonialList"
 
 const App = () => {
   return (
-    <div className='bg-[#0e0c1e]'>
+    <div className="bg-[#0e0c1e]">
       <Navbar />
       <Hero />
       <About />
       <Services />
       <Work />
       <RatingTestimonial />
-      <TestimonialList />
+      <TestimonialList showPending={true} />
       <Contact />
-      <ChatBot /> 
+      <ChatBot />
     </div>
   )
 }
